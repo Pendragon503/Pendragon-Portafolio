ec532c1fbfca709ee06e80ffe88693a7685660d7
William Martínez <WILLIAMSAN78@LIVE.COM>
2026-05-30 00:27:37 -0600

    Add gallery images and Harbar portfolio styles
    Add multiple gallery and profile images (Fotografias/*, img/*) and update styles to introduce a new "Harbar" portfolio UI. CSS changes set image aspect-ratios/object-fit, add a responsive harbar-style header, hero, profile, projects, gallery and contact layouts, and a contact-frame. Update SASS partials (_contact.scss, _projects.scss) and add _harbar.scss; rebuild main.scss. Minor JS updates (js/main.js and minified bundle) and index.html updated to accommodate the new layout and assets. This commit integrates visual assets and the new responsive portfolio design.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
 (function($) {
 
-	"use strict";	
+	"use strict";
 
-  
     $('.navigation').singlePageNav({
-        currentClass : 'active'
+        currentClass : 'active',
+        offset: 82
     });
-
 
     $('.toggle-menu').click(function(){
         $('.responsive-menu').stop(true,true).slideToggle();
         return false;
     });
 
+    $('.menu-toggle').click(function(){
+        var isOpen = $('.site-nav').toggleClass('is-open').hasClass('is-open');
+        $(this).attr('aria-expanded', isOpen);
+        return false;
+    });
 
+    $('.site-nav .navigation a').click(function(){
+        $('.site-nav').removeClass('is-open');
+        $('.menu-toggle').attr('aria-expanded', 'false');
+    });
 
 })(jQuery);
-
-
-
-
-
-
